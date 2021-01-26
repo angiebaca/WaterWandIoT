de5b382543eb68527de5f9fefdfe12b9c6c179e5
Angie <abaca020@fiu.edu>
2021-01-26 13:21:33 -0500

    Initial Commit

---
+++ WaterWandIoT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaterWandIoT
+//
+//  Created by Alvaro Palacios on 1/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WaterWandIoT/WaterWandIoTApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterWandIoTApp.swift
+//  WaterWandIoT
+//
+//  Created by Alvaro Palacios on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct WaterWandIoTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
