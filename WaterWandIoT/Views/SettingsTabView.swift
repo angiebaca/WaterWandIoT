b9e1419984e61670eaf05c52bdb2b348b84629b1
Angie <abaca020@fiu.edu>
2021-03-26 15:52:31 -0400

    third tab outline

--- WaterWandIoT/Views/SettingsTabView.swift
+++ WaterWandIoT/Views/SettingsTabView.swift
@@ -9,12 +9,37 @@ import SwiftUI
 
 struct SettingsTabView: View {
     
-    let gradient = Gradient(colors: [.white, .backGroundBlue])
+    let backGroundBlue = UIColor(red: 23/255, green:  138/255, blue: 180/255, alpha: 1.0)
+    
+    init() {
+        UITableView.appearance().backgroundColor = backGroundBlue
+        
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+    }
+    
+    private var itemList = [  "Add Device",
+                              "Set-up Demo",
+                              "Data Capture",
+                              "Notifications",
+                              "Contact Us"]
     
     var body: some View {
-        ZStack {
-            LinearGradient(gradient: gradient, startPoint: .top, endPoint: .bottom).ignoresSafeArea()
-            Text("SETTINGS TAB").bold()
+        NavigationView {
+            ZStack{
+                List {
+                    ForEach(self.itemList, id: \.self) { index in
+                        NavigationLink(
+                            destination: Text("\(index) Details"),
+                            label: {
+                                Text("\(index)")
+                                    .font(.system(size: 38, weight: .bold, design: .default))
+                                    .foregroundColor(.white)
+                            })
+                    }.listRowBackground(Color.backGroundBlue)
+                }
+            }
+            .navigationBarTitle("User Menu")
         }
     }
 }
